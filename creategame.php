1a31307f3dafe8930f616da11e3e1a6ca8fbe4f2
m-tsi <99395165+m-tsi@users.noreply.github.com>
2022-05-10 06:59:37 +0200

    Update creategame.php

--- creategame.php
+++ creategame.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <?php
-    echo "<html> <body>";
     
+  /*start session */
+  session_start();
 
-    /* establish connection */
-    $user='localhost';
-    $host='root';
-    $connection=new mysqli($user,$host);
-    if ($connection-> connect_error){
-        echo "Verbindung fehlgeschlagen";
-      }
+  /* establish connection */
+  $user='localhost';
+  $host='root';
+  $connection=new mysqli($user,$host);
+  if ($connection-> connect_error){
+    echo "Verbindung fehlgeschlagen";
+  }
 
-    /*check if database exists and create if needed */
-    $createdatabase="CREATE DATABASE IF NOT EXISTS databasekniffel;";
-    $connection -> query($createdatabase);
+  /*check if database exists and create if needed */
+  $createdatabase="CREATE DATABASE IF NOT EXISTS databasekniffel;";
+  $connection -> query($createdatabase);
 
-    /*set database */
-    $setdatabase=" USE databasekniffel; ";
-    $connection -> query($setdatabase);
+  /*set database */
+  $setdatabase=" USE databasekniffel; ";
+  $connection -> query($setdatabase);
 
-    /* create tables    SELECT LAST_INSERT_ID();*/
-    $createtables1=" CREATE TABLE IF NOT EXISTS gamestate(
+  /* create tables    SELECT LAST_INSERT_ID();*/
+  $createtables1=" CREATE TABLE IF NOT EXISTS gamestate(
     sessionid INT NOT NULL AUTO_INCREMENT,
     playerlist VARCHAR(300),
     PRIMARY KEY (sessionid)
-    );";
+  );";
 
 
-    $createtables2="CREATE TABLE IF NOT EXISTS player(
-        nickname VARCHAR(128),
-        playerid VARCHAR(300),
-        sessionid INT
-      );";
+  $createtables2="CREATE TABLE IF NOT EXISTS player(
+    nickname VARCHAR(128),
+    playerid VARCHAR(300),
+    sessionid INT
+  );";
     
-    $createtables3="CREATE TABLE IF NOT EXISTS gamesheet(
+  $createtables3="CREATE TABLE IF NOT EXISTS gamesheet(
     playerid VARCHAR(300),
     einser INT,
     zweier INT,
@@ -50,16 +51,15 @@
     bonus INT,
     untersumme INT,
     obersumme INT,
-    summe INT);";
+    summe INT
+  );";
 
 
   $createtables4="CREATE TABLE IF NOT EXISTS runde(
     rundeid INT NOT NULL AUTO_INCREMENT,
     playerid VARCHAR(300),
     PRIMARY KEY (rundeid)
-    );";
-
-    
+  );";
 
   $createtables5="CREATE TABLE IF NOT EXISTS wuerfe(
     rundeid INT,
@@ -67,8 +67,7 @@
     randomvalues VARCHAR(128),
     selection VARCHAR(128),
     PRIMARY KEY (rollid)
-    );";
-
+  );";
 
   /* create tables */
   $connection -> query($createtables1);
@@ -85,96 +84,76 @@
   $changestart3="ALTER TABLE runde AUTO_INCREMENT=10; ";
   $connection -> query($changestart3);
 
-
-  
-
-
   if (isset($_POST['nickname']) && $_POST['nickname']!=''){
 
-  /*read post variables */
-  /* if (isset($_POST['nickname'])){*/
-  $nickname=$_POST['nickname'];
+    /*read post variables */
+    $nickname=$_POST['nickname'];
 
-  /* create id for user */
-  $salt=random_bytes(8);
-  $time=new DateTime();
-  $timestamp= $time->getTimestamp();
-  $usernamehash=md5($salt.$nickname.$timestamp);
+    $_SESSION['nickname']=$_POST['nickname'];
+
+    /* create id for user */
+    $salt=random_bytes(8);
+    $time=new DateTime();
+    $timestamp= $time->getTimestamp();
+    $usernamehash=md5($salt.$nickname.$timestamp);
 
 
-  /*make gamestate*/
-  $makegamestateprep="INSERT INTO gamestate (playerlist) VALUES (?)";
-  $makegamestate=$connection->prepare($makegamestateprep);
-  $playerlist=json_encode([$usernamehash]);
-  $makegamestate->bind_param("s",$playerlist);
-  $makegamestate->execute();
-  $makegamestate->close();
+    /*make gamestate*/
+    $makegamestateprep="INSERT INTO gamestate (playerlist) VALUES (?)";
+    $makegamestate=$connection->prepare($makegamestateprep);
+    $playerlist=json_encode([$usernamehash]);
+    $makegamestate->bind_param("s",$playerlist);
+    $makegamestate->execute();
+    $makegamestate->close();
 
-  /*get sessionid*/
-  $getsessionidprep= "SELECT MAX(sessionid) AS sessionid FROM gamestate";
-  $getsessionid=$connection -> query($getsessionidprep);
-  $sessionid=mysqli_fetch_array($getsessionid);
-  $sessionid=$sessionid['sessionid'];
-
+    /*get sessionid*/
+    $getsessionidprep= "SELECT MAX(sessionid) AS sessionid FROM gamestate";
+    $getsessionid=$connection -> query($getsessionidprep);
+    $sessionid=mysqli_fetch_array($getsessionid);
+    $sessionid=$sessionid['sessionid'];
+    $_SESSION['sessionid']=$sessionid;
 
   
 
-  /*make first user */
-  $makeplayerprep="INSERT INTO player (nickname,playerid,sessionid) VALUES (?,?,?)";
-  $makeplayer=$connection->prepare($makeplayerprep);
-  $makeplayer->bind_param("sss",$nickname,$usernamehash,$sessionid);
-  $makeplayer->execute();
-  $makeplayer->close();
+    /*make first user */
+    $makeplayerprep="INSERT INTO player (nickname,playerid,sessionid) VALUES (?,?,?)";
+    $makeplayer=$connection->prepare($makeplayerprep);
+    $makeplayer->bind_param("sss",$nickname,$usernamehash,$sessionid);
+    $makeplayer->execute();
+    $makeplayer->close();
 
-
-  /*update session */
-  /* DELETE $newlist=  json_encode([$nickname]);
-  $updateplayersprep="UPDATE gamestate SET playerlist=? WHERE sessionid=?";
-  $updateplayers=$connection->prepare($updateplayersprep);
-  $updateplayers->bind_param("ss",$newlist,$sessionid);
-  $updateplayers->execute();
-  $updateplayers->close();*/
 
     
 
-  /* create table for new player */
-  $makegamesheetprep="INSERT INTO gamesheet (playerid) VALUES (?)";
-  $makegamesheet=$connection->prepare($makegamesheetprep);
-  $makegamesheet->bind_param("s",$usernamehash);
-  $makegamesheet->execute();
-  $makegamesheet->close();
+    /* create table for new player */
+    $makegamesheetprep="INSERT INTO gamesheet (playerid) VALUES (?)";
+    $makegamesheet=$connection->prepare($makegamesheetprep);
+    $makegamesheet->bind_param("s",$usernamehash);
+    $makegamesheet->execute();
+    $makegamesheet->close();
 
 
 
-  /*create new runde*/
-  $makerundeprep="INSERT INTO runde (playerid) VALUES (?)";
-  $makerunde=$connection->prepare($makerundeprep);
-  $makerunde->bind_param("s",$usernamehash);
-  $makerunde->execute();
-  $makerunde->close();
+    /*create new runde*/
+    $makerundeprep="INSERT INTO runde (playerid) VALUES (?)";
+    $makerunde=$connection->prepare($makerundeprep);
+    $makerunde->bind_param("s",$usernamehash);
+    $makerunde->execute();
+    $makerunde->close();
 
-  echo "Spielcode:" . $sessionid . "<br/>";
-  echo "Bitte an Spieler weitergeben!";
+    echo "Spielcode:" . $sessionid . "<br/>";
+    echo "Bitte an Spieler weitergeben!";
 
 
     
-  /* $returnsessionid= */
-  header('location: http://localhost/game.php?gameid='.$sessionid);
+    /* $returnsessionid= */
+    header('location: http://localhost/game4.html');
 
   
   
-  /*redirect to game page/waiting screen header(location: http://localhost/game.php); */
-
   }
 
   else{ 
     header('location: http://localhost/creategame.html');
-
   }
-  
-
-
-
-  echo "</body> </html>";
-    
 ?>
